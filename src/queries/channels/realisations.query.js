3835695ba7127b66fca676ca75c1f1954b26a63b
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2025-04-17 08:36:53 +0200

    Update queries for multi-site

--- src/queries/channels/realisations.query.js
+++ src/queries/channels/realisations.query.js
@@ -1,11 +1,8 @@
 import { imageQuery } from "@/queries/components/image.query";
-import { buttonsQuery } from "@/queries/components/buttons.query";
-import { heroQuery } from "@/queries/components/hero.query";
-import { SITE } from "@/enums/site";
 
 export const realisationsQuery = ({ service }) => `
     query MyQuery {
-      realisations: realisationsEntries(sites: ["${SITE.bouwwerken}"] ${service ? ', service: ["${service}"]' : ""}) {
+      realisations: realisationsEntries(site: "${process.env.NEXT_PUBLIC_SITE}" ${service ? ', service: ["${service}"]' : ""}) {
         ... on realisation_Entry {
           id
           heading
